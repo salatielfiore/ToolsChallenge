64d02d05e43bac9d39d0e669843062539a34e3c2
Salatiel Fiore <salatiel.fiore@arphoenix.com.br>
2024-07-18 17:28:21 -0300

    initial commit

---
+++ src/test/java/com/toolschallenge/ToolschallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolschallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
